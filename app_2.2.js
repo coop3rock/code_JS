74a5073f47242b45d153bb68ffbe0210fae619ce
coop3rock <102414939+coop3rock@users.noreply.github.com>
2023-08-14 22:01:44 +0300

    Delete app_2.2.js

--- app_2.2.js
+++
@@ -1,8 +0,0 @@
-// 2. Задача на фільтрування масиву
-
-// 2ий спосіб "через вбудований метод масивів"
-
-const useNames = ['Петро', 'Емма', 'Юстин', 'Ілля', 'Марта', 'Яна', 'Василь', 'Антон', 'Олена'];
-let vowels = /^[АОУИЕІЮЯЇ]/;
-let filteredNames = useNames.filter(function(name) {return vowels.test(name)});
-console.log(filteredNames);
